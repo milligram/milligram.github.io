ddf8afe5deacbb2cc7998d23206a256c27143f58
CJ Patoilo <cjpatoilo@gmail.com>
2016-10-07 13:21:01 -0300

    Changed Opbeat CDN to Bower Dependency

--- src/scripts/opbeat.js
+++ src/scripts/opbeat.js
@@ -6,7 +6,11 @@
 
 	'use strict';
 
-	if (window.location.hostname === 'localhost') return;
+	// if (window.location.hostname === 'localhost') return;
+
+	const APP_ID = 'caba356019',
+		ORG_ID = 'ed5b6ca01e3e4e6a98b6e97142e06a63',
+		OPBEAT_URL = 'bower_components/opbeat-js/opbeat.min.js';
 
 	((i, s, o, g, r, a, m, n) => {
 		n = s.createElement(o);
@@ -16,6 +20,6 @@
 		n.setAttribute('data-app-id', a);
 		m = s.getElementsByTagName(o)[document.querySelectorAll(o).length - 1];
 		m.parentNode.insertBefore(n, m);
-	})(window, document, 'script', 'https://d3tvtfb6518e3e.cloudfront.net/3/opbeat.min.js', 'ed5b6ca01e3e4e6a98b6e97142e06a63', 'caba356019');
+	})(window, document, 'script', OPBEAT_URL, ORG_ID, APP_ID);
 
 })();
