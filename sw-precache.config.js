286cc811e16558a63accd2358a1ce96e1458a8bd
CJ Patoilo <cjpatoilo@gmail.com>
2019-08-01 15:06:37 -0300

    Fix to register a ServiceWorker

--- sw-precache.config.js
+++ sw-precache.config.js
@@ -1,5 +1,9 @@
 const swPrecache = {
 	staticFileGlobs: [
+		'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
+		'https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css',
+		'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css',
+		'https://browser.sentry-cdn.com/5.5.0/bundle.min.js',
 		'dist/**.html',
 		'dist/**/*.css',
 		'dist/**/*.js',
