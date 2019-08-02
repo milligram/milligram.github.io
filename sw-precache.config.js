47e0e5cca39edf201d6fbd010a1e20911f256cf0
CJ Patoilo <cjpatoilo@gmail.com>
2019-08-01 21:19:24 -0300

    Update prettify lib from jsdelivr

--- sw-precache.config.js
+++ sw-precache.config.js
@@ -4,6 +4,7 @@ const swPrecache = {
 		'https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css',
 		'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css',
 		'https://browser.sentry-cdn.com/5.5.0/bundle.min.js',
+		'https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js',
 		'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js',
 		'dist/**.html',
 		'dist/**/*.css',
