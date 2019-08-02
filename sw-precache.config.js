b47edc0647c9326a91fa1a4a80d0b7782d855ea7
CJ Patoilo <cjpatoilo@gmail.com>
2019-08-01 21:47:52 -0300

    Remove protocolo from CDN's

--- sw-precache.config.js
+++ sw-precache.config.js
@@ -1,11 +1,11 @@
 const swPrecache = {
 	staticFileGlobs: [
-		'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
-		'https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css',
-		'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css',
-		'https://browser.sentry-cdn.com/5.5.0/bundle.min.js',
-		'https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js',
-		'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js',
+		'//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
+		'//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css',
+		'//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css',
+		'//browser.sentry-cdn.com/5.5.0/bundle.min.js',
+		'//cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js',
+		'//cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js',
 		'dist/**.html',
 		'dist/**/*.css',
 		'dist/**/*.js',
