563e6fcc5aa6a26776bdf554a6fa9b315bc6ce2b
CJ Patoilo <cjpatoilo@gmail.com>
2019-08-01 21:21:13 -0300

    Update version script removing rawgit

--- src/scripts/version.js
+++ src/scripts/version.js
@@ -4,19 +4,21 @@
 	const $description = document.querySelector('.description')
 	const $download = document.querySelector('.download')
 	const request = new window.XMLHttpRequest()
-	const repository = 'milligram/milligram'
-	let version
 
 	function onload () {
-		if (this.status < 200 && this.status >= 400) console.error('[error] There was a connection error of some sort')
-		version = JSON.parse(this.response).version
-		if ($description) $description.innerHTML = `${$description.innerHTML} <br><i><small>Currently v${version}</small></i>`
-		$download.setAttribute('href', `https://github.com/${repository}/archive/v${version}.zip`)
+		if (this.status < 200 && this.status >= 400) {
+			console.error('[error] There was a connection error of some sort')
+			return
+		}
+
+		const version = JSON.parse(this.response).version || ''
+		if ($description) $description.innerHTML = `${$description.innerHTML} <br><i><small>${$description.getAttribute('data-version')} v${version}</small></i>`
+		if ($download) $download.setAttribute('href', `https://github.com/milligram/milligram/archive/v${version}.zip`)
 	}
 
 	if (!$description || !$download) return
 
-	request.open('GET', `//cdn.rawgit.com/${repository}/master/package.json`, true)
+	request.open('GET', `//raw.githubusercontent.com/milligram/milligram/master/package.json`, true)
 	request.onload = onload
 	request.send()
 })()
