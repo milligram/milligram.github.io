bff45e1c3968ecd78a884c9dc23c88c3594272d0
CJ Patoilo <cjpatoilo@gmail.com>
2020-08-19 15:55:26 -0300

    Update milligram version

--- sw-precache.config.js
+++ sw-precache.config.js
@@ -2,7 +2,7 @@ const swPrecache = {
   staticFileGlobs: [
     'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.0/milligram.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css',
     'https://browser.sentry-cdn.com/5.5.0/bundle.min.js',
     'https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js',
